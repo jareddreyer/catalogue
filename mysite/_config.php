41bd0b82df7784753717e12db95c3ff28021555b
Jared Dreyer <jared@plasticstudio.co.nz>
2017-09-13 15:36:35 +1200

    removed Form.ss and renamed to CataloguePage after class name, added new constant to config for system root path

--- mysite/_config.php
+++ mysite/_config.php
@@ -6,9 +6,13 @@ $project = 'mysite';
 global $databaseConfig;
 require_once('conf/ConfigureFromEnv.php');
 
-//uploads folder location
-define('POSTERSDIR', 'c:\inetpub\catalogue\assets\Uploads\\');
-define('JSONDIR', 'c:\inetpub\catalogue\assets\Uploads\metadata\\');
+//set paths (NB: DO NOT ADD STARTING SLASH)
+define('SYSTEMROOT', '/var/www/catalogue/');
+define('POSTERSDIR', SYSTEMROOT.'assets/Uploads/');
+define('JSONDIR', SYSTEMROOT.'assets/Uploads/metadata\\');
+
+//logging
+SS_Log::add_writer(new SS_LogFileWriter(SYSTEMROOT.'logs/silverstripe-errors-warnings.log'), SS_Log::WARN, '<=');
 
 // Set the site locale
 i18n::set_locale('en_US');
